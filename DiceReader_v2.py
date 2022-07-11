6e43510c5378423f8a9dce4360a8cf0aaa1ef9e8
Asus imac1844 <imac1844+sourcetree@gmail.com>
2022-07-10 18:45:12 -0700

    1.0 demo, up to outlining found dice on original image.

--- DiceReader_v2.py
+++ DiceReader_v2.py
@@ -57,6 +57,13 @@ class DiceConnector:
 		self.threshold_a = 100
 		self.wrk_img = 0
 
+		self.Webcam = VideoCapture()
+		# self.Webcam.display()
+		display_window = Thread(target=self.Webcam.display())
+
+
+
+		# display_window.start()
 
 		while True:
 			try:
@@ -226,7 +233,9 @@ class VideoCapture:
 			self.frameWidth = 640
 			self.frameHeight = 480
 			self.centerPoint = (self.frameWidth/2, self.frameHeight/2)
-			self.capture = cv2.VideoCapture(1)
+			self.capture = cv2.VideoCapture(1
+
+				)
 			self.capture.set(3, self.frameWidth)
 			self.capture.set(4, self.frameHeight)
 			self.img = 0 #Defined in the "display" method every frame
@@ -258,10 +267,7 @@ class VideoCapture:
 
 
 
-
-Webcam = VideoCapture()
-display_window = Thread(target=Webcam.display())
-display_window.start()
+		
 
 
 dice_connection = DiceConnector()
