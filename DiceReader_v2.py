57cab7f5b2ff8cf95274be53a7b22692429475a5
Asus imac1844 <imac1844+sourcetree@gmail.com>
2022-07-12 21:34:36 -0700

    Added self.show, which if TRUE will display all windows. Removed threading. Added sort to contours to perform thge box search in a better way. Added individual dice. Added some attributes to Dice class.

--- DiceReader_v2.py
+++ DiceReader_v2.py
@@ -53,48 +53,27 @@ white = (255,255,255)
 
 
 
-def DC_Thread(capture_object, setup):
-	Connection = DiceConnector(capture_object)
-	if setup:
-		Connection.setup()
-		return()
-	ConThread = Thread(target=Connection.mainLoop())
-	ConThread.start()
-	# Webcam.display()
 
 class DiceConnector:
 
-	def __init__(self, Capture, ThreshTest=False):
-		self.threshold_a = 100
+	def __init__(self, Capture, frame_i, ThreshTest=False, ShowSteps=False):
+		self.threshold_a = 0
 		self.wrk_img = 0
 		self.Capture = Capture
 		self.ThreshTest = ThreshTest
-		self.defaultThresh = 220
+		self.defaultThresh = 230
+		self.max_dice = 5
 
-		self.diceFindSize = (6000, 30000)
-		self.diceOverlapSearchRadius = 10
+		self.diceFindSize = (8000, 30000)
+		self.diceOverlapSearchRadius = 3
 
+		self.show = ShowSteps # Set True to show working images
 
-		#Reference image, set by setup.
-		self.blank_arena = cv2.imread("./Images/references/blank_arena.jpg")
-
-
-
-	def setup(self):
-		#Captures empty area
-		print("Ensure area is empty and lighting is correct, then press any key to continue.")
-		cv2.waitKey()
-		_, img = self.Capture.capture.read()
-		cv2.imwrite("./Images/references/blank_arena.jpg", img)
-		print("Image saved as blank reference. Resuming...")
-
-
-	def mainLoop(self):
-		self.diceList = self.image_processor()		#coords of die bounding boxes, in form ([coords], center). Pass 'True' to print.
-		self.dice = self.find_dice()				#dice as Dice objects
+		self.diceList = self.image_processor()		#coords of die bounding boxes, in form ([coords], center). 
+		self.dice = self.list_dice()				#dice as Dice objects
 		self.display()
 
-	def find_dice(self):
+	def list_dice(self):
 		croppedDice = []
 		ogImgCenter = self.Capture.centerPoint
 		allDice = []
@@ -124,28 +103,33 @@ class DiceConnector:
 		_, image_copy = cv2.threshold(image_copy, self.threshold_a, 255, cv2.THRESH_BINARY)
 		cv2.imshow('Threshold Test(a)', image_copy)
 
-	def image_processor(self, keep=False):
-
+	def image_processor(self):
 		#Process image to a Binary Threshold
 		raw_img = self.Capture.img
 		self.wrk_img = cv2.cvtColor(raw_img, cv2.COLOR_BGR2GRAY)
 		self.wrk_img = cv2.GaussianBlur(self.wrk_img, (11,11), 1000)
 
+		if self.show:
+			cv2.imshow("1 - Blurred", self.wrk_img)
+			cv2.moveWindow("1 - Blurred", -1275,0)
+
 		
 		if self.ThreshTest:
 			cv2.imshow('Threshold Test(a)', self.wrk_img)
-			a = cv2.createTrackbar("Thres a", "Threshold Test(a)", 0, 255, self.on_change)
+			a = cv2.createTrackbar("Thresh a", "Threshold Test(a)", 0, 255, self.on_change)
 			cv2.waitKey(0)
 		else:
 			self.threshold_a = self.defaultThresh
-
-
 		_, self.wrk_img = cv2.threshold(self.wrk_img, self.threshold_a, 255, cv2.THRESH_BINARY)
+
+
+		if self.show:
+			cv2.imshow("2 -Threshold", self.wrk_img)
+			cv2.moveWindow("2 -Threshold", -1275,515)
+
+
 		canny_img = cv2.Canny(self.wrk_img, 1, 50, 2)
-
-		# cv2.imshow("canny_img", canny_img)
 		contours, _ = cv2.findContours(canny_img, 1, 2)
-
 
 		#minimum area rotated rectangle for each contour
 		minRect = [None]*len(contours)
@@ -153,7 +137,7 @@ class DiceConnector:
 			minRect[i] = cv2.minAreaRect(c)
 
 		#Drawing tools init, for debugging
-		if keep:
+		if self.show:
 			drawing = np.zeros((canny_img.shape[0], canny_img.shape[1], 3), dtype=np.uint8)
 
 
@@ -163,23 +147,27 @@ class DiceConnector:
 		boxKeep = []
 		centersList = []
 
-		for i, c in enumerate(contours):
+		SortedContours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+		for i, c in enumerate(SortedContours):
 			# Drawings for debugging
-			if keep:
+			if self.show:
 				cv2.drawContours(drawing, contours, i, red)
+
 
 			# Creates a rotated rectangle, not kept between iterations
 			box = cv2.boxPoints(minRect[i])
 			box = np.intp(box)
 			center = (int(minRect[i][0][0]),int(minRect[i][0][1]))
 			centersList.append(center)
+			
 
 			#removes duplicates
 			inList = False
 			for k in range(0, len(centersList)-1):
 				if ((centersList[k][0] - center[0])**2 + (centersList[k][1] - center[1])**2) < self.diceOverlapSearchRadius**2:
 					inList = True
-					del centersList[-1]
+					centersList.pop()
 					break
 
 			#sorts a copy of box, and keeps the sorted boxes in a list
@@ -195,39 +183,54 @@ class DiceConnector:
 			dy = sortedbox[i][1][1] - sortedbox[i][0][1]
 			boxArea = abs(dx*dy)
 
+
 			#saves (non duplicate) boxes that meet the criteria for size and ratio
 			if 0.5 < abs(dy/(dx+0.01)) < 2: #+0.01 is for 0 catching
 				if self.diceFindSize[0] < boxArea < self.diceFindSize[1]:
 					if not inList:
+
 						boxKeep.append([box, center])
-						if keep:
+						if self.show:
 							cv2.drawContours(drawing, [box], 0, green)
 							cv2.circle(drawing, center, 0, green, -1)	
-				elif keep:
-					cv2.drawContours(drawing, [box], 0, magenta)
-			elif keep:
-				cv2.drawContours(drawing, [box], 0, cyan)
-		if keep:	
-			cv2.imshow("drawing", drawing)
+
+				elif self.show:
+					# cv2.drawContours(drawing, [box], 0, magenta)
+					pass
+			elif self.show:
+				# cv2.drawContours(drawing, [box], 0, cyan)
+				pass
+		if self.show:
+			cv2.imshow("3 - Contours and boxes", drawing)
+			cv2.moveWindow("3 - Contours and boxes", -1935,515)
 		return(boxKeep)
 
 	def display(self):
 		for die, center in (self.diceList):
 			cv2.drawContours(self.Capture.img, [die], -1, green, 3)
-		cv2.imshow("Boxed Dice", self.Capture.img)
+		cv2.imshow("0 - Live Feed", self.Capture.img)
+		cv2.moveWindow("0 - Live Feed", -1935,0)
 
+		if self.show:
+			displacement = 0
+			for i, d in enumerate(self.dice):
+				cv2.imshow("Die "+str(i), d.img)
+				cv2.moveWindow("Die "+ str(i), -650,(displacement+(i*30)))
+				displacement += d.frameHeight
 
-		for i, d in enumerate(self.dice):
-			# print(d)
-			pass
-			# cv2.imshow("Die "+str(i), d)
-			# cv2.moveWindow("Die "+ str(i), -1000,0)
-		# cv2.waitKey()
+			for j in range(len(self.dice), self.max_dice):
+				try:
+					cv2.destroyWindow("Die "+ str(j))
+				except cv2.error:
+					pass
 
 class Dice:
 	def __init__(self, raw_img, center):
 		self.center = center
 		self.img = raw_img
+		self.frameWidth = len(self.img[0])
+		self.frameHeight = len(self.img)
+
 
 class VideoCapture:
 
@@ -243,22 +246,20 @@ class VideoCapture:
 
 
 		def display(self):
-			print("Display loading... (press s to save image, q to progress)")
+			print("Display loading... (press q to quit) \n")
 			frame = 0
 			while True:
+				# time.sleep(1)
 				frame += 1
 				_, img = self.capture.read()
 				self.img = img
 				if cv2.waitKey(1) & 0xFF == ord('q'):
 					break
-				if cv2.waitKey(1) & 0xFF == ord('s'):
-					print("Initializing setup...")
-					DC_Thread(self, setup = True)
-					pass
+				# if cv2.waitKey(1) & 0xFF == ord('s'):
+				# 	pass
 
-		#This is a bypass to jump directly to processing the first frame the camera pulls
-				DC_Thread(self, setup = False)
-				# cv2.imshow("Live Feed", img)
+				DiceConnector(self, frame)
+				
 
 			self.capture.release()
 			cv2.destroyAllWindows()
@@ -273,7 +274,7 @@ display_window.start()
 
 
 
-dice_connection = DiceConnector(Webcam)
+# dice_connection = DiceConnector(Webcam)
 
 # display_window.join()
 cv2.waitKey(0)
